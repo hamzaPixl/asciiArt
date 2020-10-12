037654da6dd49d57d0ce3f796792e537a9a05054
Hamza Mounir <hamza.pixelle@gmail.com>
2020-10-12 13:39:08 +0200

    add to repo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # asciiArt
 Simple stram from video flux and simply rendered in ASCII art with Python.
+
+```python3 ascii_art.py draw.jpg
+
+```python3 sscii_cam.py
+
+
+```python3 system_info
---
+++ system_info.py
@@ -0,0 +1,16 @@
+import platform
+
+# Architecture
+print("Architecture: " + platform.architecture()[0])
+
+# machine
+print("Machine: " + platform.machine())
+
+# node
+print("Node: " + platform.node())
+
+# system
+print("System: " + platform.system())
+
+# python version
+print("Python version: " + platform.python_version())
